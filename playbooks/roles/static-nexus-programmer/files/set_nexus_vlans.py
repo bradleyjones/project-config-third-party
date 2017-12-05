c9d232d2fa83a0f9f3b20dc724b5010d8f6b06fd
Sam Betts <sam@code-smash.net>
2017-12-05 13:15:24 +0000

    Make setting the nexus native vlan an optional parameter
    When programming the nexus for hosts, sometimes we want to set the
    native and other times we don't, this makes the value optional in the
    programming script.

--- playbooks/roles/static-nexus-programmer/files/set_nexus_vlans.py
+++ playbooks/roles/static-nexus-programmer/files/set_nexus_vlans.py
@@ -10,16 +10,30 @@ nexus_vlan_start = os.environ.get('NEXUS_VLAN_START')
 nexus_vlan_end = os.environ.get('NEXUS_VLAN_END')
 
 
-def clear_nexus_config(ip, user, password, intf_num, vlan_start, vlan_end):
+def clear_nexus_config(ip, user, password, intf_num, vlan_start, vlan_end,
+                       vlan_native=None):
     client = paramiko.client.SSHClient()
     client.load_system_host_keys()
     client.connect(ip, username=user, password=password)
-    cmd = ('config terminal ; '
-           'interface Ethernet {0} ; '
-           'switchport mode trunk ;'
-           'switchport trunk native vlan {1} ;'
-           'switchport trunk allowed vlan add {1}-{2} ; '
-           'vlan {1}-{2}').format(intf_num, vlan_start, vlan_end)
+
+    parameters = [intf_num, vlan_start, vlan_end]
+
+    cmd = ('config terminal ;'
+           'interface Ethernet {0} ;'
+           'switchport mode trunk ;')
+
+    if vlan_native:
+        cmd += ('switchport trunk native vlan {3} ;'
+                'switchport trunk allowed vlan add {3} ;')
+        parameters.append(vlan_native)
+
+    cmd += ('switchport trunk allowed vlan add {1}-{2} ;'
+            'vlan {1}-{2} ; ')
+
+    if vlan_native:
+        cmd += 'vlan {3}'
+
+    cmd = cmd.format(*parameters)
     print cmd
     stdin, stdout, stderr = client.exec_command(cmd)
     print stdout.readlines()
@@ -29,7 +43,7 @@ def clear_nexus_config(ip, user, password, intf_num, vlan_start, vlan_end):
 def print_usage():
     print "Usage:"
     print "    python %s <nexus-ip> <user> <password> " % sys.argv[0]
-    print "              <intf-num> <vlan-start> <vlan-end>"
+    print "              <intf-num> <vlan-start> <vlan-end> <vlan-native>"
     print "Example:"
     print "    python %s 10.0.1.32 admin MyPassword 1/9 810 813" % sys.argv[0]
     print "Note: VLAN range is inclusive."
@@ -39,8 +53,13 @@ if __name__ == '__main__':
     if "--help" in sys.argv:
         print_usage()
         sys.exit(0)
-    if len(sys.argv) != 7:
+    if (len(sys.argv) != 7 and
+            len(sys.argv) != 8):
         print_usage()
         sys.exit(1)
-    clear_nexus_config(sys.argv[1], sys.argv[2], sys.argv[3],
-                       sys.argv[4], sys.argv[5], sys.argv[6])
+    if len(sys.argv) == 7:
+        clear_nexus_config(sys.argv[1], sys.argv[2], sys.argv[3],
+                           sys.argv[4], sys.argv[5], sys.argv[6])
+    if len(sys.argv) == 8:
+        clear_nexus_config(sys.argv[1], sys.argv[2], sys.argv[3],
+                           sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
